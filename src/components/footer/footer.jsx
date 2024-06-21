6326f111bbe7aa7eadc5fdd008ebed04bf2a5d31
KaranjaK <karanjakelvin77@gmail.com>
2024-06-21 10:36:53 +0300

    💄 Styles the Footer component

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,26 +5,26 @@ import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <Container>
-        <Row className="align-items-center">
-          {/* <MailchimpForm /> */}
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="#"><img src={<FaLinkedin className="fa-icon"/>} alt="Icon" /></a>
-              <a href="#"><img src={<FaGithub className="fa-icon"/>} alt="Icon" /></a>
-              <a href="#"><img src={<FaTwitter className="fa-icon"/>} alt="Icon" /></a>
-            </div>
-            <p>Copyright 2022. All Rights Reserved</p>
-          </Col>
-        </Row>
-      </Container>
-    </footer>
-  )
+    return (
+        <footer className="footer">
+            <Container>
+                <Row className="align-items-center">
+                    {/* <MailchimpForm /> */}
+                    <Col size={12} sm={6}>
+                        <img src={logo} alt="Logo" />
+                    </Col>
+                    <Col size={12} sm={6} className="text-center text-sm-end">
+                        <div className="social-icon">
+                            <a href="#"><FaLinkedin className="fa-icon" /></a>
+                            <a href="#"><FaGithub className="fa-icon" /></a>
+                            <a href="#"><FaTwitter className="fa-icon" /></a>
+                        </div>
+                        <p>Copyright 2024. All Rights Reserved</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
 }
 
 export default Footer
