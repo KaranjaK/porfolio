f01e828a818cab95a4bcbffe281c034bb63b3cf0
KaranjaK <karanjakelvin77@gmail.com>
2024-07-08 18:47:45 +0300

    💄 ✨ Introduces a progress component and styles it

---
+++ src/components/skills/progress.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
+const CircularProgress = ({ targetValue, label }) => {
+  const [progressValue, setProgressValue] = useState(0);
+
+  useEffect(() => {
+    const speed = 100;
+
+    const interval = setInterval(() => {
+      setProgressValue((prev) => {
+        if (prev < targetValue) {
+          const newValue = prev + 1;
+          document.documentElement.style.setProperty(`--progress-value-${label}`, `${(newValue / 100) * 360}deg`);
+          return newValue;
+        } else {
+          clearInterval(interval);
+          return prev;
+        }
+      });
+    }, speed);
+
+    return () => clearInterval(interval);
+  }, [targetValue, label]);
+
+  return (
+    <div className="rating">
+      <div className="circular-progress" style={{ background: `conic-gradient(#7d2ae8 var(--progress-value-${label}), #ededed 0deg)` }}>
+        <h2>
+          <span className="counter" data-target={targetValue}>{progressValue}</span>
+          <sup>%</sup>
+          <br />{label}
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default CircularProgress;
--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -1,3 +1,5 @@
+import CircularProgress from './progress';
+import { useEffect, useState } from "react";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import meter1 from "../resources/meter1.svg"
@@ -5,6 +7,8 @@ import meter2 from "../resources/meter2.svg"
 import meter3 from "../resources/meter3.svg"
 import colorSharp from "../resources/color-sharp.png"
 import { Container } from 'react-bootstrap';
+import WOW from 'wowjs'
+import 'animate.css/animate.css'
 
 const Skills = () => {
     const responsive = {
@@ -26,89 +30,65 @@ const Skills = () => {
             items: 1
         }
     };
+    useEffect(() => {
+        const wow = new WOW.WOW();
+        wow.init();
+    }, [])
+
 
     return (
         <Container className="skill" id="skills">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <div className="skill-bx wow zoomIn">
-                                <h2>Skills</h2>
-                                <p>
-                                    Throughout my career, I have interacted with various industry professionals and working environments gaining various skills both technical and soft skills.
-                                    <br />
-                                    This has seen me partake in various cutting-edge web-based applications that are in use and some still in their testing stage.
-                                </p>
-                                <section className='tech-skill'>
-                                    <h1>Technical Skills</h1>
-                                    <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                                        <div className="item">
-                                            <h2><span className='counter' data-target="90">90</span><sup>%</sup><br />Python</h2>
-                                        </div>
-                                        <div className="item">
-                                            <img src={meter2} alt="Image" />
-                                            <h5>Flask</h5>
-                                        </div>
-                                        <div className="item">
-                                            <img src={meter3} alt="Image" />
-                                            <h5>Django</h5>
-                                        </div>
-                                        <div className="item">
-                                            <img src={meter1} alt="Image" />
-                                            <h5>Javascript</h5>
-                                        </div>
-                                        <div className="item">
-                                            <img src={meter3} alt="Image" />
-                                            <h5>React</h5>
-                                        </div>
-                                        <div className="item">
-                                            <img src={meter1} alt="Image" />
-                                            <h5>HTML</h5>
-                                        </div>
-                                        <div className="item">
-                                            <img src={meter1} alt="Image" />
-                                            <h5>CSS</h5>
-                                        </div>
-                                        <div className="item">
-                                            <img src={meter3} alt="Image" />
-                                            <h5>SaSS</h5>
-                                        </div>
-                                        <div className="item">
-                                            <img src={meter2} alt="Image" />
-                                            <h5>Nodejs</h5>
-                                        </div>
-                                    </Carousel>
-                                </section>
-                                <section className='soft-skill'>
-                                    <h1>Soft Skills</h1>
-                                    <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                                        <div className="item">
-                                            <img src={meter1} alt="Image" />
-                                            <h5>Effective Communication</h5>
-                                        </div>
-                                        <div className="item">
-                                            <img src={meter2} alt="Image" />
-                                            <h5>Leadership</h5>
-                                        </div>
-                                        <div className="item">
-                                            <img src={meter3} alt="Image" />
-                                            <h5>Time Management</h5>
-                                        </div>
-                                        <div className="item">
-                                            <img src={meter1} alt="Image" />
-                                            <h5>Critical Thinking</h5>
-                                        </div>
-                                        <div className="item">
-                                            <img src={meter3} alt="Image" />
-                                            <h5>Team Work</h5>
-                                        </div>
-                                    </Carousel>
-                                </section>
-                            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="skill-bx wow zoomIn">
+                            <h2>Skills</h2>
+                            <p>
+                                Throughout my career, I have interacted with various industry professionals and working environments gaining various skills both technical and soft skills.
+                                <br />
+                                This has seen me partake in various cutting-edge web-based applications that are in use and some still in their testing stage.
+                            </p>
+                            <section className='tech-skill'>
+                                <h1>Technical Skills</h1>
+                                <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider wow animate__animated fade">
+                                    <CircularProgress targetValue={98} label="HTML" />
+                                    <CircularProgress targetValue={95} label="CSS" />
+                                    <CircularProgress targetValue={92} label="SaSS" />
+                                    <CircularProgress targetValue={80} label="JavaScript" />
+                                    <CircularProgress targetValue={90} label="JavaScript" />
+                                    <CircularProgress targetValue={90} label="Python" />
+                                </Carousel>
+                            </section>
+                            <section className='soft-skill'>
+                                <h1>Soft Skills</h1>
+                                <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                                    <div className="item">
+                                        <img src={meter1} alt="Image" />
+                                        <h5>Effective Communication</h5>
+                                    </div>
+                                    <div className="item">
+                                        <img src={meter2} alt="Image" />
+                                        <h5>Leadership</h5>
+                                    </div>
+                                    <div className="item">
+                                        <img src={meter3} alt="Image" />
+                                        <h5>Time Management</h5>
+                                    </div>
+                                    <div className="item">
+                                        <img src={meter1} alt="Image" />
+                                        <h5>Critical Thinking</h5>
+                                    </div>
+                                    <div className="item">
+                                        <img src={meter3} alt="Image" />
+                                        <h5>Team Work</h5>
+                                    </div>
+                                </Carousel>
+                            </section>
                         </div>
                     </div>
                 </div>
-                <img className="background-image-left" src={colorSharp} alt="Image" />
+            </div>
+            <img className="background-image-left" src={colorSharp} alt="Image" />
         </Container>
     )
 }
